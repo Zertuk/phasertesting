3856f119820e2c41f9b6739af79575e6588ad21d
Justin <zertukis@gmail.com>
2014-07-30 00:06:28 -0500

    added score text, keeps track of total in top left and displays the score awarded from each jump next to player for a short period

--- js/game.js
+++ js/game.js
@@ -5,7 +5,13 @@ var cursors;
 var star;
 var jumpNoise;
 var starLoc;
+var style;
+var starScore;
+var score = 0;
+var scoreText;
+var scoreToAdd = 10;
 var starCount = 0;
+
 function preload() {
 		game.load.spritesheet('backgrounds', 'assets/backgrounds.png', 231, 69);
 		game.load.image('platform', 'assets/platform.png');
@@ -39,13 +45,14 @@ function create() {
 		}
 
 		this.timer = this.game.time.events.loop(500, addStars, this);
+		this.scoreTimer = this.game.time.events.loop(1000, scoreWipe, this);
 
 		dude = game.add.sprite(180, game.world.height - 100, 'dude');
 
 		game.physics.arcade.enable(dude);
 
 		dude.body.bounce.y = 0;
-		dude.body.gravity.y = 300;
+		dude.body.gravity.y = 400;
 		dude.body.collideWorldBounds = true;
 
 		dude.animations.add('left', [0, 1, 2 , 3], 10, true);
@@ -57,6 +64,15 @@ function create() {
 		cursors = game.input.keyboard.createCursorKeys();
 
 		jumpNoise = game.add.audio('noise');
+
+		style = {font : "30px Arial", fill: "#ff0044"}
+		scoreText = game.add.text(5, 5, "0", style);
+		scoreText.fixedToCamera = true;
+		
+		starScore = game.add.text(dude.world.x - 5, dude.world.y - 5, "", style);
+
+
+
 
 
 }
@@ -87,10 +103,26 @@ function update() {
 
 function starJump(dude, star) {
 	star.kill();
-	dude.body.velocity.y = -300;
+
+	dude.body.velocity.y = -350;
 	starCount = starCount - 1;
 	jumpNoise.play();
+	
+	addScore(star);
 	diamondCheck();
+}
+
+function addScore(star) {
+	scoreWipe();
+	scoreToAddText(star);
+	if (star.key == "diamond") {
+		score = score*2;
+	}
+	else {
+		score = score + scoreToAdd;
+		scoreToAdd = scoreToAdd + 10;
+	}
+	scoreText.setText(score);
 
 }
 
@@ -104,18 +136,36 @@ function addStars() {
 	if (starCount < 50) {
 		var rand = Math.random()*300;
 		var star = stars.create(rand, starLoc, 'star');
-		starLoc = starLoc - 100;
+		starLoc = starLoc - 75;
 		starCount = starCount + 1;
 		game.physics.arcade.enable(star);
-		star.body.velocity.y = 25;
+		star.body.velocity.y = 40;
 	}
 }
 
 function diamondCheck() {
-	var rand = Math.floor(Math.random()*10);
+	var rand = Math.floor(Math.random()*20);
 	console.log(rand);
 	if (rand == 1) {
 		var diamond = stars.create(0, dude.world.y - 500, 'diamond');
-		diamond.body.velocity.x = 25;
+		diamond.body.velocity.x = 30;
 	}
+	if (rand === 2) {
+		var diamond = stars.create(400, dude.world.y - 500, 'diamond');
+		diamond.body.velocity.x = -30;
+	}
+}
+
+function scoreToAddText(star) {
+	if (star.key == 'diamond') {
+		starScore = game.add.text(dude.world.x - 5, dude.world.y - 5, "2x!", style);
+	}
+	else {
+		starScore = game.add.text(dude.world.x - 5, dude.world.y - 5, scoreToAdd, style);
+	}
+}
+
+function scoreWipe() {
+	starScore.destroy();
+
 }
