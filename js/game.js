f305310a1e623d07e7854de99805a2c3a04ac017
Justin <zertukis@gmail.com>
2014-07-31 20:13:23 -0500

    added sound for bird, player now moves via mouse instead of keys, adjusted values to make more difficult

--- js/game.js
+++ js/game.js
@@ -1,13 +1,14 @@
-var game = new Phaser.Game(400, 600, Phaser.AUTO, 'game_container', { preload: preload, create: create, update: update });
+var game = new Phaser.Game(600, 600, Phaser.AUTO, 'game_container', { preload: preload, create: create, update: update });
 var dude;
 var platform;
 var cursors;
 var star;
 var jumpNoise;
-var starLoc;
+var starLoc = -300;
 var style;
 var starScore;
 var score = 0;
+var purrNoise;
 var scoreText;
 var scoreToAdd = 10;
 var starCount = 0;
@@ -19,12 +20,13 @@ function preload() {
 		game.load.image('space', 'assets/space.png');
 		game.load.image('moon', 'assets/moon.png');
 		game.load.image('platform', 'assets/platform.png');
-		game.load.spritesheet('dude', 'assets/space cat.png', 32, 32);
+		game.load.spritesheet('dude', 'assets/space cat 2.0.png', 32, 32);
 		game.load.spritesheet('cat', 'assets.cat2,png', 32, 32);
 		game.load.image('star', 'assets/yarn.png');
 		game.load.audio('noise', 'assets/noise.wav');
 		game.load.spritesheet('diamond', 'assets/bird.png', 32, 32);
 		game.load.audio('music', 'assets/copycat.mp3');
+		game.load.audio('purr', 'assets/purr.wav');
 }
 
 function create() {
@@ -34,15 +36,16 @@ function create() {
 
 
 		var sky = game.add.sprite(0, 7500, 'sky');
-		sky.scale.setTo(1.0, 125);
+		sky.scale.setTo(2.0, 125);
 
 		var uppersky = game.add.sprite(0, 4000, 'uppersky');
-		uppersky.scale.setTo(1.0, 125);
+		uppersky.scale.setTo(2.0, 125);
 
 		var cloud = game.add.sprite(0, 7530, 'cloud');
+		cloud.scale.setTo(1.5, 1.0)
 
 		var space = game.add.sprite(0, 100, 'space');
-		space.scale.setTo(1.0, 1.0);
+		space.scale.setTo(2.0, 1.0);
 
 		var moon = game.add.sprite(0, 0, 'moon');
 
@@ -67,14 +70,15 @@ function create() {
 		// this.timer = this.game.time.events.loop(750, addStars, this);
 		// this.scoreTimer = this.game.time.events.loop(1000, scoreWipe, this);
 
-		dude = game.add.sprite(180, game.world.height - 100, 'dude');
+		dude = game.add.sprite(180, game.world.height - 130, 'dude');
 
 		game.physics.arcade.enable(dude);
 
 		dude.body.bounce.y = 0;
-		dude.body.gravity.y = 400;
+		dude.body.gravity.y = 375;
 		dude.body.collideWorldBounds = true;
-		dude.scale.setTo(3.0, 3.0);
+		dude.anchor.setTo(0.5, 0.5);
+		dude.scale.setTo(1.75, 1.75);
 
 		dude.animations.add('right', [0, 1, 2], 10, true);
 		dude.animations.add('left', [0, 1, 2] ,10, true);
@@ -92,6 +96,8 @@ function create() {
 		mainMusic = game.add.audio('music');
 		mainMusic.play();
 
+		purrNoise = game.add.audio('purr');
+
 		style = {font : "30px Arial", fill: "#ff0044"}
 		scoreText = game.add.text(5, 5, "0", style);
 		scoreText.fixedToCamera = true;
@@ -102,34 +108,37 @@ function create() {
 
 function update() {
 
+
 	game.physics.arcade.collide(dude, platform);
 	game.physics.arcade.overlap(dude, stars, starJump, null, this);
 	game.physics.arcade.overlap(stars, platform, starDie, null, this);
-	if (cursors.left.isDown) {
-		dude.body.velocity.x = -150;
-		dude.animations.play('left');
-	}
-	else if (cursors.right.isDown) {
-		dude.body.velocity.x = 150;
-		dude.animations.play('right');
-	}
-	else {
-		dude.body.velocity.x = 0;
-		dude.animations.stop();
-		dude.frame = 4;
-	}
+	movePlayer();
+	// if (Phaser.Pointer.clientX > dude.world.x) {
+	// 	game.physics.arcade.moveToPointer(dude, 1000);
+	// 	dude.animations.play('left');
+	// }
+	// else if (cursors.right.isDown) {
+	// 	dude.body.velocity.x = 150;
+	// 	dude.animations.play('right');
+	// }
+	// else {
+	// 	dude.body.velocity.x = 0;
+	// 	dude.animations.stop();
+	// 	dude.frame = 4;
+	// }
+
+
 
 	if (cursors.up.isDown) {
-		dude.body.velocity.y = -300;
+		dude.body.velocity.y = -400;
 	}
 }
 
 function starJump(dude, star) {
 	star.kill();
 
-	dude.body.velocity.y = -350;
+	dude.body.velocity.y = -375;
 	starCount = starCount - 1;
-	jumpNoise.play();
 	
 	addScore(star);
 	diamondCheck();
@@ -140,10 +149,12 @@ function addScore(star) {
 	scoreToAddText(star);
 	if (star.key == "diamond") {
 		score = score*2;
+		purrNoise.play();
 	}
 	else {
 		score = score + scoreToAdd;
 		scoreToAdd = scoreToAdd + 10;
+		jumpNoise.play();
 	}
 	scoreText.setText(score);
 
@@ -156,17 +167,17 @@ function starDie(star) {
 }
 
 function addStars() {
-		var rand = Math.random()*300;
+		var rand = Math.random()*500 + 20;
 		var star = stars.create(rand, starLoc, 'star');
-		starLoc = starLoc - 75;
+		star.scale.setTo(1, 1);
+		starLoc = starLoc - 130;
 		starCount = starCount + 1;
-		game.physics.arcade.enable(star);
-		star.body.velocity.y = 40;
+		star.body.velocity.y = 25;
 }
 
 
 function diamondCheck() {
-	var rand = Math.floor(Math.random()*20);
+	var rand = Math.floor(Math.random()*30);
 	console.log(rand);
 	if (rand == 1) {
 		var diamond = stars.create(0, dude.world.y - 500, 'diamond');
@@ -193,5 +204,18 @@ function scoreToAddText(star) {
 
 function scoreWipe() {
 	starScore.destroy();
+
+}
+
+function movePlayer() {
+	if (game.input.mousePointer.x > dude.x + 25) {
+		dude.body.velocity.x = 500;
+	}
+	else if (game.input.mousePointer.x < dude.x - 25) {
+		dude.body.velocity.x = -500;
+	}
+	else {
+		dude.body.velocity.x = 0;
+	}
 
 }
